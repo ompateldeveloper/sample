8e623a921e714577d91805c09a9c3a6ce6bf972f
Om Patel <115335915+ompateldeveloper@users.noreply.github.com>
2024-06-11 18:11:55 +0530

    Revert "changes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
 import Button from "./Button"
-import Navbar from "./Navbar"
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <div className="my-grid mt-10 px-4 sm:px-8 lg:px-32 grid grid-cols-1  gap-4 sm:grid-cols-2 md:grid-cols-4">
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-        <Button>Click Me</Button>
-      </div>
+      <Button>
+        about
+      </Button>
+      <Button>
+        home
+      </Button>
     </div>
   )
 }
